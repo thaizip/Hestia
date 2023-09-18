6bdbd46f41eb6fcdfa9355df7ed0a9b7627db767
Paulo Ricardo <ricardo.consul77@gmail.com>
2023-09-18 01:15:42 -0300

    Inicio Prj hestia

---
+++ hestia/src/main/java/prjIntegrador/hestia/HestiaApplication.java
@@ -0,0 +1,13 @@
+package prjIntegrador.hestia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HestiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HestiaApplication.class, args);
+	}
+
+}
