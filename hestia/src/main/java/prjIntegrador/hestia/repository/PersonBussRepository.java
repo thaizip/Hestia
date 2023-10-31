294c8e605842ac78704c7215eefee5ee6f435cb0
Paulo Ricardo <ricardo.consul77@gmail.com>
2023-10-31 11:21:47 -0300

    exclusão de arquivos que são de outra funcionalidade

--- hestia/src/main/java/prjIntegrador/hestia/repository/PersonBussRepository.java
+++
@@ -1,7 +0,0 @@
-package prjIntegrador.hestia.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import prjIntegrador.hestia.entity.PersonBUSS;
-
-public interface PersonBussRepository extends JpaRepository<PersonBUSS, Integer> {
-}
